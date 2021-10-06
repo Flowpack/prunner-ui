ae1cc225182d7a5007fc77e508c581a8deef97fb
Berit Hlubek <berit.hlubek@networkteam.com>
2021-10-06 13:57:50 +0200

    Start with colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,35 @@ module.exports = {
   theme: {
     fontFamily: {
       'sans': '"Noto Sans", sans-serif',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: {
+        DEFAULT: '#fff',
+      },
+      black: {
+        DEFAULT: '#000',
+      },
+      blue: {
+        light: '#00b5ff',
+        DEFAULT: '#00b5ff',
+        dark: '#00b5ff',
+      },
+      green: {
+        light: '#00a338',
+        DEFAULT: '#00a338',
+        dark: '#00a338',
+      },
+      gray: {
+        700: '#323232',
+        600: '#3f3f3f',
+        DEFAULT: '#999',
+        400: '#c4c4c4',
+      },
+      orange: {
+        DEFAULT: '#ff8700',
+      }
     }
   }
 };
